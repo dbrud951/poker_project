2399fda68351f5bb089f560b6783acab6a0a9444
SuJoung Kim <abbey17@naver.com>
2017-06-19 22:45:25 +0900

    Card 클래스에 랭크비교테스트 추가

--- src/main/java/NoSuchRankException.java
+++ src/main/java/NoSuchRankException.java
@@ -2,5 +2,5 @@
  * Created by SUDALKIM on 2017-06-19.
  */
 public class NoSuchRankException extends RuntimeException{
-    
+
 }
--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -4,11 +4,22 @@
 
 import org.junit.Test;
 
-@Test
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+
 public class CardTest {
-    public void 카드_7_스페이드 생성(){
+
+    @Test
+    public void 카드_7_스페이드_생성() {
         Card card = new Card(7, Suit.SPADES);
         assertThat(card.getRank(), is(7));
         assertThat(card.getSuit(), is(Suit.SPADES));
     }
+
+    @Test(expected = NoSuchRankException.class)
+    public void 카드의_랭크는_13이하여야한다() {
+        new Card(14, Suit.CLUBS);
+    }
+
 }
