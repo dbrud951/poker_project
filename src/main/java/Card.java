d7878142c73f71a7d578abafb8553e06eb3fefbd
SuJoung Kim <abbey17@naver.com>
2017-06-19 22:14:00 +0900

    카드 클래스 테스트 생성

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -2,4 +2,5 @@
  * Created by 정명용 on 2017-06-19.
  */
 public class Card {
+
 }
--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -2,4 +2,9 @@
  * Created by 정명용 on 2017-06-19.
  */
 public class CardTest {
+    public void 카드_7_스페이드 생성(){
+        Card card = new Card(7, Suit.SPADE);
+        assertThat(card.getRank(), is(7));
+        assertThat(card.getSuit(), is(Suit.SPADE));
+    }
 }
