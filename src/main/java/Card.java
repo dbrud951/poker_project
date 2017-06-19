8cbfbbaf99e0eba055f0913ff40182432c71cfcc
SuJoung Kim <abbey17@naver.com>
2017-06-19 22:23:41 +0900

    카드 클래스 생성자 생성

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -3,4 +3,13 @@
  */
 public class Card {
 
+    private int rank;
+    private Suit suit;
+
+    public Card(int rank, Suit suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
+
+
 }
--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -7,8 +7,8 @@ import org.junit.Test;
 @Test
 public class CardTest {
     public void 카드_7_스페이드 생성(){
-        Card card = new Card(7, Suit.SPADE);
+        Card card = new Card(7, Suit.SPADES);
         assertThat(card.getRank(), is(7));
-        assertThat(card.getSuit(), is(Suit.SPADE));
+        assertThat(card.getSuit(), is(Suit.SPADES));
     }
 }
