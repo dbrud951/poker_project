00423612cb3f1262bbf5d242485a1e7fdec0460a
Yang Daeun <wt3030@hanmail.net>
2017-06-19 23:05:53 +0900

    PokerType enum 클래스에 코드 추가

--- src/main/java/PokerType.java
+++ src/main/java/PokerType.java
@@ -2,4 +2,15 @@
  * Created by user on 2017-06-19.
  */
 public enum PokerType {
+    FIVE(5),
+    SEVEN(7);
+    private int numberOfCard;
+
+    PokerType(int numberOfCard) {
+        this.numberOfCard = numberOfCard;
+    }
+    public int getNumberOfCard() {
+        return this.numberOfCard;
+    }
+
 }
