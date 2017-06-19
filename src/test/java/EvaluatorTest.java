bbb71e2b27914af27031af121b267bcb24a77eac
Yang Daeun <wt3030@hanmail.net>
2017-06-20 02:01:19 +0900

    포카드 테스트 추가

--- src/test/java/EvaluatorTest.java
+++ src/test/java/EvaluatorTest.java
@@ -26,6 +26,20 @@ public class EvaluatorTest {
     }
 
     @Test
+    public void RANK가_4개가동일하면_포카드다() {
+        Evaluator evaluator = new Evaluator();
+        List<Card> cardList = Arrays.asList(
+                new Card(10,Suit.DIAMONDS),
+                new Card(10,Suit.HEARTS),
+                new Card(10,Suit.SPADES),
+                new Card(10,Suit.CLUBS),
+                new Card(2,Suit.DIAMONDS)
+        );
+        Ranking result = evaluator.evaluate(cardList);
+        assertThat(result, is(Ranking.FourOfaKind));
+    }
+
+    @Test
     public void RANK가_2개_3개가_동일하면_풀하우스다() {
         Evaluator evaluator = new Evaluator();
         List<Card> cardList = Arrays.asList(
