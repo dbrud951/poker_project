061d828369afb21c982bcdf562b41ff3fb2abea4
SuJoung Kim <abbey17@naver.com>
2017-06-19 22:21:02 +0900

    suit 클래스 재생성 후 ranking 클래스 생성

---
+++ src/main/java/Ranking.java
@@ -0,0 +1,18 @@
+/**
+ * Created by SUDALKIM on 2017-06-19.
+ */
+public enum Ranking {
+    Nopair,
+    OnePair,
+    TwoPairs,
+    ThreeOfaKind,
+    Straight,
+    BackStraight,
+    Mountian,
+    Flush,
+    FullHouse,
+    FourOfaKind,
+    StraightFlush,
+    BackStraightFlush,
+    RoyalStraightFlush;
+}
