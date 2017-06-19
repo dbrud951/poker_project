1a24525b6098d2f7efe23a9c94450fd6c88a9921
Yang Daeun <wt3030@hanmail.net>
2017-06-19 23:13:44 +0900

    DeckTest에 Deck 2개 테스트 추가

--- src/test/java/DeckTest.java
+++ src/test/java/DeckTest.java
@@ -1,9 +1,19 @@
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 /**
  * Created by user on 2017-06-19.
  */
 public class DeckTest {
 
-
+    @Test
+    public void Deck2개의_총카드수는_104(){
+        Deck deck = new Deck(2);
+        int totalCard = deck.getTotalCard();
+        assertThat(totalCard, is(104));
+    }
 
 
 
