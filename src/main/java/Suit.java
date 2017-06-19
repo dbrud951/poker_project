a30a53e43fbac1354866111f5c38b2dd521aecc2
SuJoung Kim <abbey17@naver.com>
2017-06-19 22:16:28 +0900

    suit 클래스 생성

---
+++ src/main/java/Suit.java
@@ -0,0 +1,9 @@
+/**
+ * Created by SUDALKIM on 2017-06-19.
+ */
+public class Suit {
+    SPADES,
+    HEARTS,
+    CLUBS,
+    DIAMONDS;
+}
--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -1,6 +1,10 @@
 /**
  * Created by 정명용 on 2017-06-19.
  */
+
+import org.junit.Test;
+
+@Test
 public class CardTest {
     public void 카드_7_스페이드 생성(){
         Card card = new Card(7, Suit.SPADE);
